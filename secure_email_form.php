9cd16c2364ab91e7deba0898647a7082f09051f0
zugate <just.chill.sit@gmail.com>
2018-10-09 02:14:46 +0530

    added form to mail

--- secure_email_form.php
+++ secure_email_form.php
@@ -5,6 +5,9 @@ if($_POST["addr"]==""||$_POST["phone"]==""||$_POST["fvname"]==""||$_POST["lvname
 echo "Fill All Fields..";
 }else{
 // Check if the "Sender's Email" input field is filled out
+$fname=$_POST['fvname'];
+$lname=$_POST['lvname'];
+$message=$_POST['msg'];
 $email=$_POST['vemail'];
 // Sanitize E-mail Address
 $email =filter_var($email, FILTER_SANITIZE_EMAIL);
@@ -15,9 +18,9 @@ echo "Invalid Sender's Email";
 }
 else{
 $subject = "wish to contact"
-$message = $_POST['msg'];
-$headers = 'From:'. $email . "rn"; // Sender's Email
-$headers = 'Cc:'. $email . "rn"; // Carbon copy to Sender
+$message = $fname." ".$lname."\n".$_POST['msg'];
+$headers = 'From:'. $email; // Sender's Email
+$headers = 'Cc:'. $email; // Carbon copy to Sender
 // Message lines should not exceed 70 characters (PHP rule), so wrap it
 $message = wordwrap($message, 70);
 // Send Mail By PHP Mail Function
